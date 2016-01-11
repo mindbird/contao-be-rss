1aa4087443b534b6b86fbd5f43f382db021d275f
Wusch <florian.otto@mindbird.de>
2016-01-11 18:01:18 +0100

    Initial

--- contao/dca/tl_settings.php
+++ contao/dca/tl_settings.php
@@ -3,10 +3,10 @@
 if (substr($GLOBALS['TL_DCA']['tl_settings']['palettes']['default'], - 1) != ';') {
     $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';';
 }
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= '{be_changelog_legend},be_changelog_src;';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= '{be_rss_legend},be_rss_src;';
 
-$GLOBALS['TL_DCA']['tl_settings']['fields']['be_changelog_src'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_settings']['be_changelog_src'],
+$GLOBALS['TL_DCA']['tl_settings']['fields']['be_rss_src'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['be_rss_src'],
     'exclude' => true,
     'inputType' => 'text',
     'eval' => array(
